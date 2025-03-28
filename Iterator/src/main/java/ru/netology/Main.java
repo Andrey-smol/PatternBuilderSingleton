e4a5efa954ae38ceaaabf51323bf105f90ee1074
Andrey-smol <andreyksmol@yandex.ru>
2025-03-28 17:39:02 +0300

    main.c was corrected

--- Iterator/src/main/java/ru/netology/Main.java
+++ Iterator/src/main/java/ru/netology/Main.java
@@ -1,15 +1,16 @@
 package ru.netology;
 
 public class Main {
+    private static final int MIN_RANDOM_NUMBER = 80;
+    private static final int MAX_RANDOM_NUMBER = 100;
     public static void main(String[] args) {
 
         int count = 0;
-        for (int r : new Randoms(90, 100)) {
+        for (int r : new Randoms(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER)) {
             System.out.println("Случайное число: " + r);
             count++;
-            if (r == 100) {
-                System.out.println("Выпало число 100, давайте на этом закончим");
-                System.out.println("Count = " + count);
+            if (r == MAX_RANDOM_NUMBER) {
+                System.out.printf("Выпало число %d через %d проходов, давайте на этом закончим\n", MAX_RANDOM_NUMBER, count);
                 break;
             }
         }
