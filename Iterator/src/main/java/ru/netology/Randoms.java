352c8788fd0f3c14709b9e799d157367e555a6db
AKuzavov <ingener@intechnology.ru>
2025-03-28 08:52:56 +0300

    iterator was added, readme.md was changed

---
+++ Iterator/src/main/java/ru/netology/Randoms.java
@@ -0,0 +1,34 @@
+package ru.netology;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer>{
+//    private final Random random;
+//    int min;
+//    int max;
+    Iterator<Integer> iterator;
+
+    public Randoms(int min, int max) {
+//        random = new Random();
+//        this.min = min;
+//        this.max = max + 1;
+        iterator = new Random().ints(min, max + 1).iterator();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return iterator.next();
+                //return random.nextInt(min, max);
+            }
+        };
+    }
+}
